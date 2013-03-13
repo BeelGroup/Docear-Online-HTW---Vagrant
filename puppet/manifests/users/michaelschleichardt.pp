a2be988e0f4d3e556c8367aa568071f2cdc29c40
Paul Stüber <paul.stueber@web.de>
2013-03-13 20:49:41 +0100

    spelling

--- puppet/manifests/users/michaelschleichardt.pp
+++ puppet/manifests/users/michaelschleichardt.pp
@@ -1,7 +1,7 @@
-add_user { "michaelschleichart":
-  username => "michaelschleichart",
-  full_name => "Michael Schleichart",
-  home => "/home/michaelschleichart",
+add_user { "michaelschleichardt":
+  username => "michaelschleichardt",
+  full_name => "Michael Schleichardt",
+  home => "/home/michaelschleichardt",
   groups => ["sudo"],
   ssh_key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDCAxBVJryFsgIrdRz5TpgTmZ2GRnNfXhA2czfEucDJAcAJ5I+1t0gXUeBa9OGZu6bB/xuyu3FyjqvilWRrWfCehYBXc9uxK8wp0rxzbTN9RzTPFPB1tm0MPPENhLeV6N1mqBhhMT88toQw37P+AXDNUuA3W40wIelccnarMDii7X86BzlefXHs11dFQ5jdMUOFCoOky800dksxhOeHBgMUIqw+TalJ5J2XPtLrsgeWS/IjTVHjEAdhWlX/gPENFgnIar98cn47JKv3qnC6DidPueYz+uLMH1dNftgYZvMKldY6RqCZJlmtZ+4RMr4zHNwaQM4r96my60TD1tfFSOoz", 
   ssh_key_type => "rsa"
