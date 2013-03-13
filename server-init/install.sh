79fa2d34ecf97bc30b8f2b0426dbfd4f66a7514d
Michael Schleichardt <michael@schleichardt.net>
2013-03-13 13:26:28 +0100

    update install script for root

--- server-init/install.sh
+++ server-init/install.sh
@@ -1,11 +1,14 @@
- 
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user. Call with sudo." 2>&1
+  exit 1
+fi
 
 #http://docs.puppetlabs.com/guides/installation.html#installing-from-gems-not-recommended
-sudo apt-get remove --assume-yes puppet
-sudo apt-get --assume-yes autoremove 
-sudo apt-get install --assume-yes rubygems && \
-sudo gem install --no-rdoc --no-ri puppet -v 2.7.19 && \
-sudo puppet resource group puppet ensure=present && \
-sudo puppet resource user puppet ensure=present gid=puppet shell='/sbin/nologin' && \
-sudo cp /var/lib/gems/1.8/gems/puppet-2.7.19/conf/auth.conf /etc/puppet/auth.conf && \
-sudo puppet --version
+apt-get remove --assume-yes puppet
+apt-get --assume-yes autoremove
+apt-get install --assume-yes rubygems && \
+gem install --no-rdoc --no-ri puppet -v 2.7.19 && \
+puppet resource group puppet ensure=present && \
+puppet resource user puppet ensure=present gid=puppet shell='/sbin/nologin' && \
+cp /var/lib/gems/1.8/gems/puppet-2.7.19/conf/auth.conf /etc/puppet/auth.conf && \
+puppet --version
