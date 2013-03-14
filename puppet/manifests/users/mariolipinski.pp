4d67c1a7ae1f94eef6c7a5e0c8487e02e869b599
Michael Schleichardt <michael@schleichardt.net>
2013-03-14 17:58:32 +0100

    mario with dsa ssh key

--- puppet/manifests/users/mariolipinski.pp
+++ puppet/manifests/users/mariolipinski.pp
@@ -4,5 +4,5 @@ add_user { "mariolipinski":
   home => "/home/mariolipinski",
   groups => ["sudo"],
   ssh_key => "AAAAB3NzaC1kc3MAAACBAK1mLGPrhQTuF6CbMT9j0dIRqgUmEP1AODyPfRXdZ1CUlxrkz+YyKqZLOFojpG5TyOSGlNfaibneUMFXhF8SFil92grePMOfjQGus/NPkM6JIGTkIKyvTtfU4sYOmNmxdOBvn7Kp5GTuueHyOFz0BaZNmR/qfmJKRt2W+cJkCDjVAAAAFQCvdmxY2mby7UbccEuwoBkUmJ+dfwAAAIA9XuJuqK9pAidh9+rDXD6nh9ldijlN/LxBeui6c8uP/Z/m61bt+MaDCzZ31HDaPeqz5l9A3UORJiURuVPD08LllY55JHVjXgQsQkGWhj7YOrPAgyt2qNp4//JBjfhx1iJwOovzdFjkgJ3r960bQW5NVncy7Ji7GyX5UVh3LIjzPQAAAIBgcpDWYVFqwV8n/zV5SMuzYBIJX5E+pEFXAp1fSxAf7uGpEAy11J86Tajcg7ZN3PXo6eVOYBAWLzf6/NKix11dk5rdgP3kdPtC9EKNzvi2Rh/5GVFRA/85CQiPhoB2/O5cDexzc3HkWwXrTjfGfQgfCQfoR6MBmtbE9giMxoPBJA==",
-  ssh_key_type => "rsa"
+  ssh_key_type => "dsa"
 }
