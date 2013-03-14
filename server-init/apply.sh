c650d010dc162e5e92301837c105ff4604717f57
Michael Schleichardt <michael@schleichardt.net>
2013-03-14 16:51:05 +0100

    stoping services before applying Puppet

--- server-init/apply.sh
+++ server-init/apply.sh
@@ -7,6 +7,8 @@ deploy_env=prod
 if [ `hostname` == "v22013031569111142.yourvserver.net" ]; then
     deploy_env="staging"
 fi
+sudo service play-frontend stop
+sudo service mindmap-backend stop
 export FACTER_deploy_environment=$deploy_env
 export FACTER_stuff_folder='/root/puppet-stuff'
 puppet apply --modulepath "${FACTER_stuff_folder}/puppet/modules" "${FACTER_stuff_folder}/puppet/manifests/default.pp"
