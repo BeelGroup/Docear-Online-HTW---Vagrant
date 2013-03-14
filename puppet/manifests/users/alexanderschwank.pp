b381c5afbb455d2cdee5b9b599761ca37aa2f650
Paul Stüber <paul.stueber@web.de>
2013-03-14 09:59:29 +0100

    added user pp for alex & julius

---
+++ puppet/manifests/users/alexanderschwank.pp
@@ -0,0 +1,8 @@
+add_user { "alexanderschwank":
+  username => "alexanderschwank",
+  full_name => "Alexander Schwank",
+  home => "/home/alexanderschwank",
+  groups => ["sudo"],
+  ssh_key => "AAAAB3NzaC1yc2EAAAABIwAAAQEAq27TafJBGx0clLWzm26MLtXgv+8+Gzl9yndWNsXQq3L9Xu6hzKeZ4t3TuMcJcm5bHw4NsNDXgHcDuvJWnJCdjR6TXc/OnBmnKGaONA/sPaf5iF2//JJ7tIDz6ic0j5vAsSohrt/pEzKwR8xtgSl1pTchZogxAOPg2isYlQjSgWput579OoUO54fWuZtAIvA+V0wHObUFxKUW8j/obHty4V+4AetVAhyFjRthOoJD87i3Y0kMVeym0WZpwlRmmQB/SmyLTO2TEJh+RyYdPEGIFCFu84ADIS+3iEVkgtiqefJ8sCfRAU3d3hRDueqS/vz1aDZEHfZ5VzYhSiIEzEWxow==", 
+  ssh_key_type => "rsa"
+}
