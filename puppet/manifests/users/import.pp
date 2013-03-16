da013cc9fca57876e10c29d0cd29e3f84ea43668
Michael Schleichardt <michael@schleichardt.net>
2013-03-16 01:39:21 +0100

    added import user for Jenkins

---
+++ puppet/manifests/users/import.pp
@@ -0,0 +1,7 @@
+add_user { "import":
+    username => "import",
+    full_name => "import, user that is used to upload artifacts",
+    home => "/home/import",
+    ssh_key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQDdKChnI4w3HquzVZvzHjtoSEboKsvy400oy8TiMdI8kjP+/wrLQWPYD90Pe2lk9iIc1DbtlNelWpSc+9tz8SJh9TUJ/W5nFlQr2x2lQURvxZngoV/xIKVNRG31GqYtyRBm2BxP64lUz8sSJiHMOkQYno1RMi6gKX5wfU7NHPybsmxUrlU+HZlmk8Y5rFNNnYSf2fjboGHv2vJyXO5IGNDBPxzb7I8V5GeuLR3+u/VljfnY8VWT//z5u2+G5021CTRqySI85FPlN8tJjDbXUn581Oo95TUSypzDDcaNY7VDfN2Sn8wp3VVAxQR45mLKipVMUJSpqXK34h7V/0IjIT4P",
+    ssh_key_type => "rsa"
+}
