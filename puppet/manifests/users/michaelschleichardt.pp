0ad455409f7f6accca145c4bcc56e2b26e6317be
Michael Schleichardt <michael@schleichardt.net>
2013-03-16 01:51:08 +0100

    removed runtime errors: Invalid parameter is_root

--- puppet/manifests/users/michaelschleichardt.pp
+++ puppet/manifests/users/michaelschleichardt.pp
@@ -5,5 +5,4 @@ add_user { "michaelschleichardt":
   groups => ["sudo"],
   ssh_key => "AAAAB3NzaC1kc3MAAACBAKeBDC1RUar7rxCRfRVd2jwgrUNKUWRp7VEjeAsoK95JFuXeW2ylc5kDEcWYMDBWxK9/3vUgrD/C+MLviSnWEXUu2alVvkho9lc4nkfvzhuVuDkr2WcmROMNZn/g+IbWB4g8otOb3Q7h2cTXU8KsRS5uCUmdcXlGxmLNygLu14d3AAAAFQDJTpf9mTEzMK8NYYNQbJAVcjP82wAAAIBBBmxUc5SPgTdcKYMwp/ehXMosVoh9E64LF+AFDwm2kmPLMqHra3cEUETgF+hK2Ok2Cf8WKiNxyKKOQjfLG5BioCq8IeGBeHQd9NtwJKN7ifQDoFgbu6QPes2841X5Hc1yp9sSp0NBLVBlu37mJpRFodiUzNY2MBh8xIhGOnFqQQAAAIEAiX1Lc+Ge5mn1C8d6RMbD21/bUxeeJYqjSrFpCZW5WKGJMaHjsPYqx6/KgFgQrA81V0HO0YkZaw6f9yBuk9DDkcsKrmXp7Ljvcl4qxsTvcbAljHjl9ySr1d0kucaY6WaAVTc+wgzh9jrskMvMSfqzZpj1vMzwrEsSl2EoOoFRizs=",
   ssh_key_type => "dsa",
-  is_root => true
 }
