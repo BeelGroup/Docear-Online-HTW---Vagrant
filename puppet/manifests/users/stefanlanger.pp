7e78b385cb193afc60bfaf33b7ddae4d3ed57d67
Michael Schleichardt <michael@schleichardt.net>
2013-06-02 11:44:02 +0200

    changed username stefanlanger to stefan

--- puppet/manifests/users/stefanlanger.pp
+++ puppet/manifests/users/stefanlanger.pp
@@ -1,7 +1,7 @@
-add_user { "stefanlanger":
-  username => "stefanlanger",
+add_user { "stefan":
+  username => "stefan",
   full_name => "Stefan Langer",
-  home => "/home/stefanlanger",
+  home => "/home/stefan",
   groups => ["sudo"],
   ssh_key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCpLKWOVYBYnCirmxQaB7fsKS6TbKkf0Dt3Dsc/FpaSAKwumgHULrndxHSctCdsKj4kJtjxESZ/xbF16NROWmSMVz8OTbpb/4wG0HtIZhFQwFpQ1GdHxr4viBd59XnOsGPqQJxm8FCdHmP/Ln6PETjYhY7D7ZXRIkToNZkYW0uPHz1I06xI1tKWs899ZswtZEdmDFd8u0tYAAvqRjrQmuqKGPvv7IIuEF1Jf/4Z7eX1A676n0vlaejxDAvjm3/MJnG/mKZOWNIFfNQydwaWQkF0zq+Q8VtpOO7Z56sFau/4811yNB8dq2GagRbWlYQsumJytBGRsopsRqdT3pB65/Q7",
   ssh_key_type => "rsa"
